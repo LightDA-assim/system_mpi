3a556d2087a3a5ed70c436a4de014b2aedeeaeb0
John Haiducek <jhaiduce@gmail.com>
2021-01-29 15:03:23 -0500

    Added compiler flags to provide MPI feature information to preprocessor

--- cmake/modules/UseMPI.cmake
+++ cmake/modules/UseMPI.cmake
@@ -1,5 +1,13 @@
 find_package(MPI REQUIRED)
 
+if(MPI_Fortran_HAVE_F90_MODULE)
+  add_definitions(-DHAVE_MPI_F90_MODULE)
+endif()
+
+if(MPI_Fortran_HAVE_F08_MODULE)
+  add_definitions(-DHAVE_MPI_F08_MODULE)
+endif()
+
 function(configure_mpi_target target)
   target_compile_definitions(${target} PRIVATE ${MPI_Fortran_COMPILE_DEFINITIONS})
   target_compile_options(${target} PRIVATE ${MPI_Fortran_COMPILE_OPTIONS})
