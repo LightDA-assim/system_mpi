335acaf3b3e396e03194143f38fa4901783d9b0f
John Haiducek <jhaiduce@gmail.com>
2023-07-12 15:01:45 -0400

    Add MPI group types to mpi_types.h

--- src/mpi_types.h
+++ src/mpi_types.h
@@ -2,8 +2,10 @@
 #define MPI_REQUEST_TYPE type(MPI_Request)
 #define MPI_STATUS_TYPE type(MPI_Status)
 #define MPI_COMM_TYPE type(MPI_Comm)
+#define MPI_GROUP_TYPE type(MPI_Group)
 #else
 #define MPI_REQUEST_TYPE integer
 #define MPI_STATUS_TYPE integer, dimension(MPI_STATUS_SIZE)
 #define MPI_COMM_TYPE integer
+#define MPI_GROUP_TYPE integer
 #endif
